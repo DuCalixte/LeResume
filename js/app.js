d84f99020a1d39780398daa3f5f586d2afdf6090
Stanley Calixte <DuCalixte@gmail.com>
2015-06-29 19:09:25 -0400

    update file as pre-compiled jsx

--- js/app.js
+++ js/app.js
@@ -1,31 +1,7 @@
+/** @jsx React.DOM */
 'use strict;'
 
-var Resume = React.createClass({
-    render: function() {
-        var data = (function() {
-            var _data = null;
-            var self = this;
-            $.ajax({
-                url: 'data/le_resume.json',
-                type: 'get',
-                error: function(data) {
-                    console.log(data);
-                },
-                success: function(data) {
-                    self._data = jQuery.parseJSON(data); //do something with data
-                }
-            });
-            return _data;
-        })();
-        return (
-            React.createElement('div', {
-                className: "main"
-            }, null)
-        );
-    }
-});
-
 React.render(
-    React.createElement(Resume, null),
+    <Resume url={'../data/le_resume.json'}/>,
     document.body
 );
