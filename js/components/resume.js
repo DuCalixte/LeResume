4017fb434ae2ea02c7c1037414cecfaede46b563
Stanley Calixte <DuCalixte@gmail.com>
2015-07-08 22:52:58 -0400

    update existing component

--- js/components/resume.js
+++ js/components/resume.js
@@ -2,12 +2,21 @@
 'use strict;'
 
 var Resume = React.createClass({
+    getDefaultProps: function(){
+        return {
+            interval: 14400
+        };
+    },
     loadResumeData: function() {
         $.ajax({
             url: this.props.url,
             dataType: 'json',
+            cache: false,
             success: function(data) {
               this.setState({data: data});
+            }.bind(this),
+            error: function(xhr, status, err){
+                console.error(this.props.url, status, err.toString());
             }.bind(this)
         });
     },
@@ -16,6 +25,8 @@ var Resume = React.createClass({
     },
     componentWillMount: function() {
         this.loadResumeData();
+        console.log(this.props.interval);
+        setInterval(this.loadResumeData, this.props.interval);
     },
 	render: function(){
         var data=this.state.data;
@@ -24,6 +35,7 @@ var Resume = React.createClass({
             <About about={data.about} />
             <NavBar />
             <WorkExperience experiences={data.experiences}/>
+            <Education universities={data.universities} certifications={data.certifications} />
             <LivingLocations locations={data.locations} />
         </div>);
 	}
